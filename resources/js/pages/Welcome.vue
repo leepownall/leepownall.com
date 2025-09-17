73f8778d316bc063921cabe38ff7dadbeb19a188
Lee Pownall <lee@pownall.uk>
2025-09-17 21:24:41 +0100

    wip

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -3,17 +3,20 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <Head ">
+    <Head>
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="px-4 sm:px-8 max-w-4xl prose prose-slate">
+    <div class="prose max-w-4xl px-4 prose-slate sm:px-8">
         <header>
             <h2>Lee Pownall</h2>
         </header>
         <main>
             <p>A Senior Developer from the West Midlands. Currently building things with Laravel.</p>
-            <p>We can connect on  <a href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a>, <a href="https://github.com/leepownall" target="_blank">GitHub</a> or email at <a href="mailto:lee@pownall.uk">lee@pownall.uk</a>.</p>
+            <p>
+                We can connect on <a href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a>,
+                <a href="https://github.com/leepownall" target="_blank">GitHub</a> or email at <a href="mailto:lee@pownall.uk">lee@pownall.uk</a>.
+            </p>
         </main>
     </div>
 </template>
