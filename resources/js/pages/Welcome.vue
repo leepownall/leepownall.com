4613fec7886cbc1daa3c0f51d61193f092199e8d
Lee Pownall <lee@pownall.uk>
2025-09-17 20:43:21 +0100

    Init

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="p-4 sm:p-8 max-w-3xl prose prose-slate">
+        <header>
+            <h1>Lee Pownall</h1>
+        </header>
+        <main>
+            <p class="lead">A Senior Developer from the West Midlands. I specialise in Laravel and focus on writing clean, maintainable, and testable code. Currently a fan of the TALL stack.</p> 
+        </main>
+    </div>
+</template>
