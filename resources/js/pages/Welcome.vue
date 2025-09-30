7a219ce20a0288ed07b05dc33098d9e3f05f1f36
Lee Pownall <lee@pownall.uk>
2025-09-30 16:12:44 +0100

    wip

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,17 +1,18 @@
-<script setup lang="ts">
-import { Head } from '@inertiajs/vue3';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div class="prose max-w-4xl p-4 prose-slate sm:p-8">
+    <div class="prose max-w-2xl p-4 prose-slate sm:p-8">
         <header>
             <h1>Lee Pownall</h1>
         </header>
         <main>
-            <p class="text-xl">A Senior Developer from the West Midlands. Currently building things with Laravel. I also enjoy running, currently focusing on 5k, 10k and half marathon distances. </p>
+            <p class="text-xl">A Senior Developer from the West Midlands. I am currently building things with Laravel.</p>
+            <p class="text-lg">I also enjoy running, currently focusing on 5k, 10k and half marathon distances.</p>
+            <p class="text-lg">Some code can be seen on <a href="https://github.com/leepownall" target="_blank">GitHub</a>.</p>
             <p class="text-lg">
-                We can connect on <a href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a>,
-                <a href="https://github.com/leepownall" target="_blank">GitHub</a> or email at <a href="mailto:lee@pownall.uk">lee@pownall.uk</a>.
+                We can connect on
+                <a href="https://www.linkedin.com/in/lee-pownall" target="_blank">LinkedIn</a> or email me at
+                <a href="mailto:lee@pownall.uk">lee@pownall.uk</a>.
             </p>
         </main>
     </div>
